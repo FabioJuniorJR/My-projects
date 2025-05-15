9556db95515485f5c7aa0b8a995fbec0de22bd7e
Fábio Adriano Corrêa de Oliveira Júnior <94453492+FabioJuniorJR@users.noreply.github.com>
2025-05-15 10:27:28 -0300

    Add files via upload

---
+++ API Rest DepartamentoUsuario/userdepartment/src/main/java/com/desenvolvedor/userdepartment/UserdepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolvedor.userdepartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserdepartmentApplication.class, args);
+	}
+
+}
